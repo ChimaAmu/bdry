266159f96aa5744c023302f0d80379febacabbd2
salad <chimaamu@proton.me>
2026-04-10 17:11:14 +0100

    Simplify variable name for current day

--- bdry.sh
+++ bdry.sh
@@ -8,29 +8,29 @@ usage() {
     exit 0
 }
 
+today="$(date +%Y%m%d)"
 case $1 in 
     -a | -add )
         shift;
-        today="$(date +%Y%m%d)"
         if [ ! -f "$today" ] ;
         then
             touch "$today"
             date +%A,_%dth_%B_%Y | sed s/_/\ /g > "$today"
             echo "------------------------------" >> "$today"
         fi
-        echo "$(date +%T)" "$@" >> "$(date +%Y%m%d)"
+        echo "$(date +%T)" "$@" >> "$today"
         exit 0
         ;;
     -d | -delete )
-        sed -i "$2"'d' "$(date +%Y%m%d)"
+        sed -i "$2"'d' "$today"
         exit 0
         ;;
     -p | -print )
         if [ "$2" = "number" ] || [ "$2" = "num" ] || [ "$2" = "n" ]; then
-            cat -n "$(date +%Y%m%d)"
+            cat -n "$today"
             exit 0
         fi
-        cat "$(date +%Y%m%d)"
+        cat "$today"
         exit 0
         ;;
     -h | * )
